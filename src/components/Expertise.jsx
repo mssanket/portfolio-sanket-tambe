8c864c972458e42a96c23ff9d6ca24edd998ea93
mssanket <mssanket@gmail.com>
2024-09-18 23:24:29 +0530

    Make website responsive and ad map

--- src/components/Expertise.jsx
+++ src/components/Expertise.jsx
@@ -13,10 +13,10 @@ const Expertise = () => {
                             backgroundSize: "cover",
                         }
                     }
-                    className='box-container items-center flex py-16'>
-                    <div className='flex text-white justify-center'>
+                    className='box-container items-center flex flex-col md:flex py-16'>
+                    <div className='flex text-white justify-center w-full md:1/2 px-5'>
                         {/* Text container */}
-                        <div className='w-2/3 text-center space-y-4'>
+                        <div className='w-full text-center space-y-4'>
                             <h1 className='text-4xl font-bold '>I love these technologies</h1>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
                                 In rerum suscipit eum praesentium quasi velit dolor
@@ -25,10 +25,32 @@ const Expertise = () => {
                             <button className=' text-2xl px-4 py-2 bg-orange-500 rounded-full shadow-lg'>Hire Me</button>
                         </div>
                     </div>
-                    <div className='flex justify-center'>
+                    <div className='flex justify-center w-full md:w-1/2 px-5 mt-10 md:mt-0'>
                         {/* Skills container */}
-                        <div className=' justify-center w-2/3 flex h-fit space-x-3 flex-wrap'>
-                            <p className='bg-gray-300 w-fit px-3 py-2 mt-2 rounded-full hover:bg-orange-500 cursor-pointer'>Core Java</p>
+                        <div className='flex justify-center w-full flex-wrap gap-3'>
+
+                            {
+                                [
+                                    "Core Java",
+                                    "J2EE",
+                                    "Hibernate (ORM Tool)",
+                                    "Spring (Framework)",
+                                    "Spring Boot Framework",
+                                    "Spring Security",
+                                    "Javascript",
+                                    "React JS",
+                                    "SQL",
+                                ].map((skill, index) => (
+                                    <p
+                                        key={index}
+                                        className='bg-gray-300 px-3 py-2 mt-2 rounded-full hover:bg-orange-500 cursor-pointer'
+                                    >
+                                        {skill}
+                                    </p>
+                                ))
+                            }
+
+                            {/* <p className='bg-gray-300 w-fit px-3 py-2 mt-2 rounded-full hover:bg-orange-500 cursor-pointer'>Core Java</p>
                             <p className='bg-gray-300 w-fit px-3 py-2 mt-2 rounded-full hover:bg-orange-500 cursor-pointer'>J2EE</p>
                             <p className='bg-gray-300 w-fit px-3 py-2 mt-2 rounded-full hover:bg-orange-500 cursor-pointer'>Hibernate (ORM Tool)</p>
                             <p className='bg-gray-300 w-fit px-3 py-2 mt-2 rounded-full hover:bg-orange-500 cursor-pointer'>Spring Framework</p>
@@ -36,7 +58,7 @@ const Expertise = () => {
                             <p className='bg-gray-300 w-fit px-3 py-2 mt-2 rounded-full hover:bg-orange-500 cursor-pointer'>Spring Security</p>
                             <p className='bg-gray-300 w-fit px-3 py-2 mt-2 rounded-full hover:bg-orange-500 cursor-pointer'>Javascript</p>
                             <p className='bg-gray-300 w-fit px-3 py-2 mt-2 rounded-full hover:bg-orange-500 cursor-pointer'>ReactJS</p>
-                            <p className='bg-gray-300 w-fit px-3 py-2 mt-2 rounded-full hover:bg-orange-500 cursor-pointer'>SQL</p>
+                            <p className='bg-gray-300 w-fit px-3 py-2 mt-2 rounded-full hover:bg-orange-500 cursor-pointer'>SQL</p> */}
                         </div>
                     </div>
                 </div>
